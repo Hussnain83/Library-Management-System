b7ac36d4b4773766378f99d9a26ba393efecf1b6
Hussnain83 <hussnaindogar2006@gmail.com>
2025-09-04 19:24:38 +0500

    adding netlify domain

--- Backend/server.js
+++ Backend/server.js
@@ -5,31 +5,30 @@ import db from "./db.js";
 import cors from "cors";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-  origin: ["https://library-management-system-dogar-js.vercel.app",
-  "https://library-management-system-git-main-hussnain-dogars-projects.vercel.app",
-"http://127.0.0.1:5500",
-"https://library-management-system-hussnain-dogars-projects.vercel.app" ]
-}));
-
+app.use(
+  cors({
+    origin: [
+      "https://library-management-systemss.netlify.app",
+      "http://127.0.0.1:5500",
+    ],
+  })
+);
 
 import userRoutes from "./routes/userRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js"
-import borrowRoutes from "./routes/borrowRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js";
+import borrowRoutes from "./routes/borrowRoutes.js";
 app.use("/user", userRoutes);
 app.use("/book", bookRoutes);
 app.use("/", borrowRoutes);
-
 
 // Test route
 app.get("/", (req, res) => {
   res.send("✅ Library Management Backend is running on Railway!");
 });
 
-
-app.listen(PORT, "0.0.0.0", ()=>{
-    console.log("listening on port",PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("listening on port", PORT);
 });
